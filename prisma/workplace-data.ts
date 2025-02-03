7a2ec61f17f4d1766d6dc56c4b8f62a4fad0bc75
Stephen Han <stephenhan.dev@gmail.com>
2025-02-03 19:30:37 +1100

    added update-locations.ts script

--- prisma/workplace-data.ts
+++ prisma/workplace-data.ts
@@ -1,46 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { Client } from '@googlemaps/google-maps-services-js';
+import { googleMapsClient } from '../src/server/services/googleMapsClient'
 
 const prisma = new PrismaClient()
-const googleMapsClient = new Client({});
-
-async function initializeWorkplaces() {
-  // 기존 데이터 삭제 (선택사항)
-  await prisma.location.deleteMany();
-
-  // Get Place IDs for each location
-  const workplaces = await Promise.all([
-    getPlaceIdAndCreate({
-      name: 'Sorrel Cafe & Bar',
-      company: 'Juncafe Opera Pty Ltd',
-      address: 'Broadway Shopping Centre K34, K34/1 Bay St, Glebe NSW 2037, Australia'
-    }),
-    getPlaceIdAndCreate({
-      name: 'Baskin Robbins',
-      branch: 'Circular Quay',
-      company: 'Ice Opera Pty Ltd',
-      address: 'Quay Grand, Lot 2/61-63 Macquarie St, Sydney NSW 2000, Australia'
-    }),
-    getPlaceIdAndCreate({
-      name: 'Baskin Robbins',
-      branch: 'Manly',
-      company: 'Ice Opera Pty Ltd',
-      address: '53 East Esplanade, The Corso, Manly NSW 2095, Australia'
-    }),
-    getPlaceIdAndCreate({
-      name: 'Sushi Roll',
-      company: 'Top Ryde Sushiroll Pty Ltd',
-      address: 'Shop 3042, Ground Level, Devlin St, Ryde NSW 2112, Australia'
-    }),
-    getPlaceIdAndCreate({
-      name: 'Home',
-      company: 'Stephen & Irene Co',
-      address: 'unit 12/50 Audley St, Petersham NSW 2049, Australia'
-    })
-  ]);
-
-  console.log('지점 데이터 생성 완료:', workplaces);
-}
 
 async function getPlaceIdAndCreate(locationData: any) {
   const response = await googleMapsClient.findPlaceFromText({
@@ -62,9 +23,45 @@ async function getPlaceIdAndCreate(locationData: any) {
   });
 }
 
-initializeWorkplaces()
+async function main() {
+  await prisma.location.deleteMany(); // Clear existing data
+
+  const locations = await Promise.all([
+    getPlaceIdAndCreate({
+      name: 'Sorrel Cafe & Bar',
+      company: 'Juncafe Opera Pty Ltd',
+      address: 'Shop K333-334, Level 3, Broadway Sydney1 Bay St. Broadway NSW 2007'
+    }),
+    getPlaceIdAndCreate({
+      name: 'Baskin Robbins',
+      branch: 'Circular Quay',
+      company: 'Ice Opera Pty Ltd',
+      address: 'Shop 4, Lot 2 Quay Grand 61-63 Macquarie St. Sydney NSW 2000'
+    }),
+    getPlaceIdAndCreate({
+      name: 'Baskin Robbins',
+      branch: 'Manly',
+      company: 'Ice Opera Pty Ltd',
+      address: '53 East Esplanade, The Corso, Manly NSW 2095, Australia'
+    }),
+    getPlaceIdAndCreate({
+      name: 'Sushi Roll',
+      company: 'Top Ryde Sushiroll Pty Ltd',
+      address: 'Shop 3042, Ground Level, Devlin St, Ryde NSW 2112, Australia'
+    }),
+    getPlaceIdAndCreate({
+      name: 'Home',
+      company: 'Stephen & Irene Co',
+      address: 'unit 12/50 Audley St, Petersham NSW 2049, Australia'
+    })
+  ]);
+
+  console.log('Created locations:', locations);
+}
+
+main()
   .catch((e) => {
-    console.error('지점 데이터 생성 중 에러 발생:', e)
+    console.error('Error creating locations:', e)
     process.exit(1)
   })
   .finally(async () => {
